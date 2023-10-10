4f497ed1be745fcd1bcee1bf6d7eb710a9752eca
Karolayne Silva <96891482+Karolayne-silva@users.noreply.github.com>
2023-10-09 21:52:13 -0300

    adicionando scripts e deixando a tela orçamento responsiva

--- js/script.js
+++ js/script.js
@@ -26,3 +26,32 @@ nebulaRadio.addEventListener('click', () => {
     contNimbus.style.display = "none";
     contMagic.style.display = "none";
 });
+
+// ativar links do menu
+const links = document.querySelectorAll('.navegacao a')
+
+function ativarLink(link){
+    const url = location.href;
+    const href = link.href;
+
+    if (url.includes(href)){
+        link.classList.add('ativo');
+    }
+}
+
+links.forEach(ativarLink);
+
+//ativar items do orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro){
+    const elemento = document.getElementById(parametro)
+    if(elemento){
+        elemento.checked = true
+    }
+    
+    console.log(elemento);
+}
+
+parametros.forEach(ativarProduto);
